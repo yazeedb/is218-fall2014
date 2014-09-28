ff4d39d0b1c5af8817047ba84a9786ac05229672
yazman12 <yazman12@gmail.com>
2014-09-28 00:05:42 -0400

    Styled tables

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<head>
+	<style>
+		body {
+		}
+		table {
+			border: solid 1px black;
+			border-collapse: collapse;
+		}
+		th {
+			color: white;
+			background-color: #555555;
+			padding-right: 30px;
+			text-align: left;
+		}
+		td {
+			padding: 0 10px;
+			text-align: left;
+		}
+	</style>
+</head>
+
 <?php
 	class fileReader {
 		function readFile($filename) { //file name parameter, allows any file to be entered
@@ -20,14 +41,33 @@
 				foreach($rows as $row) {
 					$i++;
 					$universityNum = $i - 1;
-					echo '<a href="' . $_SERVER['PHP_SELF'] . '/?university=' . $universityNum . '">' . $row['INSTNM'] . '</a>' . "<br>";
+					echo '<a href="' . $_SERVER['PHP_SELF'] . '/?university=' . $universityNum . '">' . $row["INSTNM"] . '</a>' . "<br>";
 				}
 			}
 
 			$row = $rows[$_GET["university"]];
+
+			/*echo "<pre>";
+				foreach($rows as $row) {
+					var_dump($row);
+				}
+			echo "</pre>";*/
+
+			echo "<table>";
 			foreach($row as $key => $value) {
-				echo $key . ": " . $value . "<br>";
+					echo "<tr>";
+
+					echo "<th>";
+						echo $key;
+					echo "</th>";
+
+					echo "<td>";
+						echo $value;
+					echo "</td>";
+
+					echo "</tr>";
 			}
+			echo "</table>";
 
 		} //end function
 	} //end class
