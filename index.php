1bbd92e5a4e369caa2bef4be70d71d78e1cbb93b
yazman12 <yazman12@gmail.com>
2014-10-01 18:58:06 -0400

    Added class with static methods–HTML Utility belt
    New HTML utility belt file used to dynamically generate links and tables for CSV file using static methods

--- index.php
+++ index.php
@@ -1,49 +1,53 @@
-<?php
-	class File
-	{
-		private $fileName;
-		public $rows = array();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-		public function __construct($fileName) {
-			$this->fileName = $fileName;
-		} //end __construct function
+<body>
+	<div>
+		<?php
+			include('htmlUtility.php');
+			class File
+			{
+				private $fileName;
+				public function __construct($fileName) {
+					$this->fileName = $fileName;
+				} //end __construct function
 
-		public function read() {
-			$first_column = TRUE;
-			ini_set('auto_detect_line_endings', TRUE);
+				public function read() {
+					$first_column = TRUE;
+					ini_set('auto_detect_line_endings', TRUE);
 
-			if(($handle = fopen($this->fileName, "r")) !== FALSE) {
-				while($row = fgetcsv($handle, ",")) {
-					if($first_column) {
-						$column_header = $row;
-						$first_column = FALSE;
-					} else {
-						$rows[] = array_combine($column_header, $row);
+					if(($handle = fopen($this->fileName, "r")) !== FALSE) {
+						while($row = fgetcsv($handle, ",")) {
+							if($first_column) {
+								$column_header = $row;
+								$first_column = FALSE;
+							} else {
+								$rows[] = array_combine($column_header, $row);
+							}
+						}
+						fclose($handle);
+					} //end fopen if
+					$this->rows = $rows;
+				} //end read function
+
+				public function printRows() {
+					$rows = $this->rows;
+					if(empty($_GET)) {
+						Html::makeLink($rows);
 					}
-				}
-				fclose($handle);
-			} //end fopen if
-			$this->rows = $rows;
-		} //end read function
+					$row = $rows[$_GET['university']];
+					Html::makeTable($row);
+				} //end printRows function
+				
+			} //end class File
 
-		public function printRows() {
-			$rows = $this->rows;
-			if(empty($_GET)) {
-				foreach($rows as $row) {
-					$i++;
-					$universityNum = $i - 1;
-					echo '<a href="' . $_SERVER['PHP_SELF'] . '/?university=' . $universityNum . '">' . $row["INSTNM"] . '</a>' . '<br>';
-				}
-			} //end $_GET if
-
-			$row = $rows[$_GET['university']];
-			foreach($row as $key => $value) {
-				echo $key . ": " . $value . "<br>";
-			}
-		} //end printRows function
-	} //end class File
-
-	$file1 = new File('schoolData.csv'); //Instantiate new File object with file parameter
-	$file1->read(); //Read the file
-	$file1->printRows(); //Print its contents
-?>
+			$file1 = new File('schoolData.csv'); //Instantiate new File object with file parameter
+			$file1->read(); //Read the file
+			$file1->printRows(); //Print its contents
+		?>
+	</div>
+</body>
+</html>
