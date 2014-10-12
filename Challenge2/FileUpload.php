2edb2bad65b5eb0e341566b6ee5d3d30482a7a0a
Yazeed Bzadough <yazman12@gmail.com>
2014-10-11 22:14:43 -0400

    Added open and close functions

--- Challenge2/FileUpload.php
+++ Challenge2/FileUpload.php
@@ -1,3 +1,15 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_errors', TRUE);
 
+	class FileUpload
+	{
+		public function fileOpen($fileName, $parameter) {
+			$handle = fopen($filename, $parameter);
+		}
+
+		public function fileClose($handle) {
+			fclose($handle);
+		}	
+	}
 ?>
