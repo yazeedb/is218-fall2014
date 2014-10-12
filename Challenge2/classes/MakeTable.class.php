8ba996b4c234671b2037cd9497c7d88b4ddce475
Yazeed Bzadough <yazman12@gmail.com>
2014-10-12 12:36:55 -0400

    Create MakeTable.class.php

---
+++ Challenge2/classes/MakeTable.class.php
@@ -0,0 +1,15 @@
+<?php
+	class MakeTable
+	{
+		public static function tableMaker(Array $record) {
+			echo '<table>';
+				foreach($record as $key => $value) {
+					echo '<tr>';
+						echo '<th>' . $key . '<th>';
+						echo '<td>' . $value . '</td>';
+					echo '</tr>';
+				}
+			echo '</table>';
+		} //end static function
+	}
+?>
