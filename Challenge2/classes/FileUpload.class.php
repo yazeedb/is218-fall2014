f1bcaf797e722c972eaf9f6323f047267d260ea1
Yazeed Bzadough <yazman12@gmail.com>
2014-10-12 10:56:30 -0400

    Update FileUpload.class.php

--- Challenge2/classes/FileUpload.class.php
+++ Challenge2/classes/FileUpload.class.php
@@ -1,15 +1,15 @@
 <?php
-	ini_set('display_errors', 1);
-	ini_set('display_errors', TRUE);
-
 	class FileUpload
 	{
-		public static function fileOpen($fileName, $parameter) {
-			$handle = fopen($filename, $parameter);
+		protected $handle;
+
+		public static function uploadFile($fileName, $mode) {
+			$handle = fopen($fileName, $mode);
+			return $handle;
 		}
 
 		public static function fileClose($handle) {
 			fclose($handle);
-		}	
+		}
 	}
 ?>
