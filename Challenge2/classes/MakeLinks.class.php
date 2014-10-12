ef82c7e94d1292f033eb3bf2d991c89240614400
Yazeed Bzadough <yazman12@gmail.com>
2014-10-12 12:36:33 -0400

    Create MakeLinks.class.php

---
+++ Challenge2/classes/MakeLinks.class.php
@@ -0,0 +1,12 @@
+<?php
+	class MakeLinks
+	{
+		public static function linkMaker(Array $records, $getVar, $universityName) {
+			$i = -1;
+				foreach($records as $record) {
+					$i++;
+					echo '<a href="?' . $getVar . '=' . $i . '">' . $record[$universityName] . '</a>' . '<br>';
+				} //end foreach
+		} //end static function
+	}
+?>
