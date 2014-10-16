56d9a88c275514c5c25e0cfacc8bb1c42a1b6237
Yazeed Bzadough <yazman12@gmail.com>
2014-10-16 16:34:43 -0400

    Create header.php

---
+++ Challenge2/includes/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	</head>
+	<body>
