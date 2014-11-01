9c2f59d3e27f42d8e4e9c98e1db278b6aa4a729e
Yazeed Bzadough <yazman12@gmail.com>
2014-11-01 16:28:22 -0400

    Update index.php

--- Challenge3/index.php
+++ Challenge3/index.php
@@ -1,30 +1,19 @@
-<head>
-	<link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="style/style.css">
-	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<h1>Twitter API</h1> <br><br>
+<?php
+	//ini_set('display_errors', 1);
+	require 'Autoloader.php';
 
-<div class="col-lg-3">
-	<div class="list-group">
-		<a href="?view=GET&url=https://api.twitter.com/1.1/statuses/home_timeline.json" class="list-group-item" id="viewTimeline">
-			View Timeline
-		</a>
-		<a href="?view=GET&url=https://api.twitter.com/1.1/statuses/user_timeline.json" class="list-group-item" id="viewTweets">
-			View Tweets
-		</a>
-		<a href="?view=GET&url=https://api.twitter.com/1.1/followers/list.json" class="list-group-item" id="viewFollowers">
-			View Followers
-		</a>
+	$header = classes\includes\header::displayHeader();
+
+	$menuOptions = classes\options::menuOptions();
+	foreach($menuOptions as $key => $value) {
+		echo classes\HtmlPrinter::printOptions($key, $value);
+	}
+?>
 	</div>
 </div>
 
 <div class="container">
 <?php
-	require 'Autoloader.php';
-	ini_set('display_errors', 1);
 	$url = $_GET['url'];
 	$url2 = 'https://api.twitter.com/1.1/statuses/update.json';
 
@@ -34,32 +23,34 @@
 	$getField = '?screen_name=yazeedbz1';
 	$postFields = $_POST;
 
-	if(!empty($_GET)) {
-		$settings = \classes\twitter_api_php\config\config::settings();
-		$userInfo = classes\TwitterHandler::get($settings, $getField, $url, $requestMethod);
+	$settings = classes\twitter_api_php\config\config::settings();
 
+	if(!empty($_GET)) {
+		$userInfo = classes\TwitterHandler::get($settings, $getField, $url, $requestMethod);
+		
 		switch ($_GET['url']) {
-			case 'https://api.twitter.com/1.1/followers/list.json':
+			case 'https://api.twitter.com/1.1/followers/list.json': 
 				echo classes\HtmlPrinter::printFollowers($userInfo);
 				break;
 
-			default:
+			case 'https://api.twitter.com/1.1/friends/list.json':
+				echo classes\HtmlPrinter::printFollowers($userInfo);
+				break;
+
+			case 'https://api.twitter.com/1.1/statuses/home_timeline.json':
 				echo classes\HtmlPrinter::printTimeline($userInfo);
 				break;
-		}
+
+			case 'https://api.twitter.com/1.1/statuses/user_timeline.json':
+				echo classes\HtmlPrinter::printTimeline($userInfo);
+				break;
+		} //end switch
 	}
 
 	if(!empty($_POST)) {
 		$userTweet = classes\TwitterHandler::post($settings, $postFields, $url2, $requestMethod2);
 		echo '<h1>Tweet successfully sent!</h1>';
 	}
-?>
-</div>
-<br> <br>
-<form action="index.php" method="POST">
-	<textarea id="status" name="status" rows="4" cols="50"></textarea> 
-	<br>
-		<input type="submit" name="submit" id="submit" value="Tweet this">
-</form>
 
-<script type="text/javascript" src="app.js"></script>
+	$footer = classes\includes\footer::displayFooter();
+?>
