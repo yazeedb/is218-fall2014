357a66d404fbe11a8b65524c3e6caff655613946
Yazeed Bzadough <yazman12@gmail.com>
2014-11-01 16:30:33 -0400

    Create config.php

---
+++ Challenge3/classes/twitter_api_php/config/config.php
@@ -0,0 +1,20 @@
+<?php
+	namespace classes\twitter_api_php\config;
+
+	class config {
+
+		public static function settings() {
+			$settings = array(
+			'oauth_access_token' => '2846540043-tt6jCs3wMFloouvuvhOKlXOzTL9KPZPEvJD05VK',
+
+			'oauth_access_token_secret' => 'hOSBJpJnEoQDvBTOlnIaL0EBM2yH5JeIxdioJ3PQnAedD',
+
+			'consumer_key' => 'KrY9GGoCeiKDONDjSeqTcn2f3',
+
+			'consumer_secret' => 'xgHnhi4Aw8TE6GBAbgyKZezaYe4fXZBZWuE8DrP4AOBbfA41rs'
+		);
+
+			return $settings;
+		} //end static function
+	}
+?>
